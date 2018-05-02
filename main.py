Fixed normalization in RaSoR model, added test for normalize_linear

main.py
@@ -147,6 +147,14 @@ def main(
         name='context_t'
         )
 
+    context_t_length = tf.placeholder(
+        tf.int32,
+        [
+            None,
+        ],
+        name='context_t_length'
+    )
+
     question_t = tf.placeholder(
         tf.int32,
         [
@@ -156,16 +164,25 @@ def main(
         name='question_t'
     )
 
+    question_t_length = tf.placeholder(
+        tf.int32,
+        [
+            None,
+        ],
+        name='question_t_length'
+    )
+
     span2position = data_ops.make_span2position(
             seq_size=max_context_len,
             max_len=max_answer_len
         )
+
     position2span = {v: k for k, v in span2position.items()}
 
     label_t = tf.placeholder(
         tf.float32,
         [
-            None, 
+            None,
             len(span2position)
         ],
         name='label_t'
@@ -174,7 +191,9 @@ def main(
     # Model outputs
     logits = model_fn(
         context_t,
+        context_t_length,
         question_t,
+        question_t_length,
         span2position,
     )
 
@@ -196,7 +215,7 @@ def main(
             checkpoint_dir=logdir,
             save_checkpoint_secs=300,
             save_summaries_steps=small_eval_every_steps
-            )
+    )
 
     # Summaries
     summary_writer = SummaryWriterCache.get(logdir)
@@ -208,7 +227,9 @@ def main(
 
     dev_feed_dict = {
             context_t: np.asarray([x['context'] for x in dev_batch]),
+            context_t_length: np.asarray([x['context_len'] for x in dev_batch]),
             question_t: np.asarray([x['question'] for x in dev_batch]),
+            question_t_length: np.asarray([x['question_len'] for x in dev_batch]),
             label_t: np.asarray([x['label'] for x in dev_batch]),
         }
 
@@ -217,7 +238,9 @@ def main(
         train_batch = next_train()
         train_feed_dict = {
             context_t: np.asarray([x['context'] for x in train_batch]),
+            context_t_length: np.asarray([x['context_len'] for x in train_batch]),
             question_t: np.asarray([x['question'] for x in train_batch]),
+            question_t_length: np.asarray([x['question_len'] for x in train_batch]),
             label_t: np.asarray([x['label'] for x in train_batch]),
         }
         current_step, train_loss, _ = sess.run(
@@ -266,7 +289,9 @@ def main(
 
             dev_small_feed_dict = {
                 context_t: np.asarray([x['context'] for x in dev_small_batch]),
+                context_t_length: np.asarray([x['context_len'] for x in dev_small_batch]),
                 question_t: np.asarray([x['question'] for x in dev_small_batch]),
+                question_t_length: np.asarray([x['question_len'] for x in dev_small_batch]),
                 label_t: np.asarray([x['label'] for x in dev_small_batch]),
             }
 

models.py
@@ -3,12 +3,10 @@ import tensorflow as tf
 import ops
 
 
-def rasor_net(context, question, span2position):
+def rasor_net(context, context_len, question, question_len, span2position):
     # Passage-aligned question representations
     context_emb = ops.embed_sequence(inputs=context, name='embedding_layer')
-    context_length = ops.unpadded_lengths(context_emb)
     question_emb = ops.embed_sequence(inputs=question, name='embedding_layer')
-    question_length = ops.unpadded_lengths(question_emb)
 
     context_dense = tf.layers.dense(
         inputs=context_emb,
@@ -21,15 +19,18 @@ def rasor_net(context, question, span2position):
     )
 
     s_passage_aligned = tf.matmul(context_dense, tf.transpose(question_dense, perm=[0, 2, 1]))
-    import code  # NOQA
-    code.interact(local=locals())
-    attn_passage_aligned = ops.sequence_softmax(s_passage_aligned, context_length)
+
+    mask_pa = tf.cast(tf.equal(s_passage_aligned, 0.0), tf.float32)
+    s_passage_aligned_exp = tf.exp(s_passage_aligned)
+    s_passage_aligned_masked = mask_pa * s_passage_aligned_exp
+    attn_passage_aligned = ops.normalize_linear(s_passage_aligned_masked)
+
     question_aligned = tf.matmul(attn_passage_aligned, question_dense)
 
     # Passage-independent question representation
     question_lstm_emb, _ = ops.bidirectional_lstm(
         inputs=question_emb,
-        input_lengths=question_length,
+        input_lengths=question_len,
         size=50,
         name='passage_independent_bLSTM'
     )
@@ -39,7 +40,11 @@ def rasor_net(context, question, span2position):
         units=1,
     )
 
-    attn_passage_independent = ops.sequence_softmax(s_passage_independent, context_length)
+    mask_pi = tf.cast(tf.equal(s_passage_independent, 0.0), tf.float32)
+    s_passage_independent_exp = tf.exp(s_passage_independent)
+    s_passage_independent_masked = mask_pi * s_passage_independent_exp
+
+    attn_passage_independent = ops.normalize_linear(s_passage_independent_masked)
 
     question_independent = attn_passage_independent * question_lstm_emb
 
@@ -52,7 +57,7 @@ def rasor_net(context, question, span2position):
     # Span representations
     context_query_aware_lstm, _ = ops.bidirectional_lstm(
         inputs=context_query_aware,
-        input_lengths=context_length,
+        input_lengths=context_len,
         size=50,
         name='context_query_aware_lstm'
     )
@@ -71,7 +76,5 @@ def rasor_net(context, question, span2position):
         units=1,
         name='final_dense_layer'
     )
-
-
 
     return tf.squeeze(logits, axis=-1)

ops.py
@@ -54,7 +54,7 @@ def unpadded_lengths(tensor):
 def normalize_linear(embedded_sequence):
     """ embedded_sequence: [batch size x example length x embeding size] """
     row_sums = tf.reduce_sum(embedded_sequence, axis=-1)
-    row_sums = tf.expand_dims(row_sums, 1) + 1e-32
+    row_sums = tf.expand_dims(row_sums, 1) + 1e-32  # prevent NaNs
 
     # Normalize
     normalized = tf.transpose(tf.divide(tf.transpose(embedded_sequence, perm=[0, 2, 1]), row_sums), perm=[0, 2, 1])

test/ops_test.py
@@ -5,34 +5,34 @@ import ops
 
 
 class BasicOpsTest(tf.test.TestCase):
-    def test_cosine_sim(self):
+    def test_normalize_linear(self):
+        """
+        Tests normalize_linear which does divides each row of M
+        by its sum, thus making each row add up to 1.
+        """
         with self.test_session():
-            A = tf.constant(np.array([[
-                [1, 1, 1, 1, 0],
-                [1, 2, 1, 0, 0],
-                [0, 1, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0]
-            ]]), dtype=tf.float32)
+            M = [[[1, 1, 1, 1, 0],
+                  [5, 5, 0, 0, 0],
+                  [5, 0, 0, 0, 0],
+                  [2, 2, 2, 2, 2],
+                  [0, 0, 0, 0, 0]],
+                 [[1, 1, 1, 1, 0],
+                  [5, 5, 0, 0, 0],
+                  [5, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0]],
+                 [[1, 1, 1, 1, 0],
+                  [5, 5, 0, 0, 0],
+                  [5, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0]]
+                 ]  # nan edge case (will lead to division by 0)
 
-            row_len = 4
-            col_len = 4
+            M = np.array(M)
+            M = tf.constant(M)
 
-            ans = A.eval()
-            ans[0, :row_len, :col_len] = np.divide(np.exp(ans[0, :row_len, :col_len]), np.sum(ans[0, :row_len, :col_len], axis=-1))
-            #
-            # mask = np.zeros_like(A.eval())
-            # mask[0, :row_len, :col_len] = 1
-            # mask = tf.constant(mask)
-            #
-            # A_exp = mask * tf.exp(A)
-            # res = ops.normalize_linear(A_exp)
-            #
-            # import code
-            # code.interact(local=locals())
-            #
-            # np.testing.assert_almost_equal(
-            #     ans,
-            #     res
-            # )
+            N = ops.normalize_linear(tf.cast(M, dtype=tf.float32))
+            n = tf.reduce_sum(N, axis=-1).eval()
+
+            np.testing.assert_almost_equal(n, np.array([[1, 1, 1, 1, 0], [1, 1, 1, 0, 0], [1, 1, 1, 0, 0]]))
 