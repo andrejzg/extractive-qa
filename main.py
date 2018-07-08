Added masked softmax to ops, fixed bug in loss was adding 1e7 instead of 1e-7 for numerical stability! and cleaned up code a bit

main.py
@@ -66,7 +66,7 @@ def run_experiment(
     id2word = {v: k for k, v in word2id.items()}
 
     # Model outputs
-    logits_t = model_fn(
+    logits_t, *the_rest = model_fn(
         context_t,
         context_t_length,
         question_t,
@@ -96,15 +96,14 @@ def run_experiment(
 
     # Negative log likelihood (i.e. multiclass cross-entropy) loss
     exp_logits_t = tf.exp(logits_t) * span_mask
-    log_sum_exp_logits_t = tf.log(tf.reduce_sum(exp_logits_t, axis=1) + 1e7)
+    log_sum_exp_logits_t = tf.log(tf.reduce_sum(exp_logits_t, axis=1) + 1e-7)
 
     gather_mask = tf.one_hot(y_preds, depth=logits_t.get_shape()[-1], dtype=tf.bool, on_value=True, off_value=False)
     y_logits = tf.boolean_mask(logits_t, gather_mask)
 
     xents = log_sum_exp_logits_t - y_logits
 
-    loss_per_example_t = xents
-    loss_t = tf.reduce_mean(loss_per_example_t)
+    loss_t = tf.reduce_mean(xents)
     tf.summary.scalar('mean_train_loss', loss_t)
 
     prediction_probs_t = exp_logits_t / tf.expand_dims(tf.reduce_sum(exp_logits_t, axis=1), 1)
@@ -154,8 +153,8 @@ def run_experiment(
             span_mask_t: np.asarray([x['span_mask'] for x in train_batch]),
             # 'out_dropout:0': dropout,
         }
-        current_step, train_loss, _xents, _logits_t, _exp_logits_t, _log_sum_exp_logits_t,  _ = sess.run(
-            [global_step_t, loss_t, xents, logits_t, exp_logits_t, log_sum_exp_logits_t, train_op],
+        current_step, train_loss, _xents, _logits_t, _exp_logits_t, _log_sum_exp_logits_t,  *_the_rest = sess.run(
+            [global_step_t, loss_t, xents, logits_t, exp_logits_t, log_sum_exp_logits_t] + the_rest + [train_op],
             feed_dict=train_feed_dict
         )
 
@@ -180,7 +179,7 @@ def run_experiment(
                         {
                             'prediction_probs_t': prediction_probs_t,
                             'label_t': label_t,
-                            'loss_per_example_t': loss_per_example_t
+                            'loss_per_example_t': xents
                         },
                         feed_dict=batch_feed_dict
                     ) for batch_feed_dict in tqdm(batched_feed_dicts)

models.py
@@ -11,10 +11,12 @@ def rasor_net(context, context_len, question, question_len, span2position, embed
     context_mask = tf.cast(tf.expand_dims(tf.sequence_mask(context_len, context.get_shape()[1].value), -1), tf.float32)
     question_mask = tf.cast(tf.expand_dims(tf.sequence_mask(context_len, question.get_shape()[1].value), -1), tf.float32)
 
+    # TODO: tie weights
     context_dense = tf.layers.dense(
         inputs=context_emb,
         units=context_emb.get_shape()[-1].value,
-        activation=tf.nn.relu
+        activation=tf.nn.relu,
+        name='ff_context_and_query'
     )
 
     question_dense = tf.layers.dense(
@@ -25,9 +27,7 @@ def rasor_net(context, context_len, question, question_len, span2position, embed
 
     s_passage_aligned = tf.matmul(context_dense, tf.transpose(question_dense, perm=[0, 2, 1]))
 
-    s_passage_aligned_exp = tf.exp(s_passage_aligned)
-    s_passage_aligned_masked = context_mask * s_passage_aligned_exp
-    attn_passage_aligned = ops.normalize_linear(s_passage_aligned_masked)
+    attn_passage_aligned = ops.masked_softmax(s_passage_aligned, context_mask)
 
     question_aligned = tf.matmul(attn_passage_aligned, question_dense)
 
@@ -45,9 +45,7 @@ def rasor_net(context, context_len, question, question_len, span2position, embed
         activation=tf.nn.relu
     )
 
-    s_passage_independent_exp = tf.exp(s_passage_independent)
-    s_passage_independent_masked = question_mask * s_passage_independent_exp
-    attn_passage_independent = ops.normalize_linear(s_passage_independent_masked)
+    attn_passage_independent = ops.masked_softmax(s_passage_independent, question_mask)
 
     question_independent = attn_passage_independent * question_lstm_emb
 
@@ -61,7 +59,7 @@ def rasor_net(context, context_len, question, question_len, span2position, embed
     context_query_aware_lstm, _ = ops.bidirectional_lstm(
         inputs=context_query_aware,
         input_lengths=context_len,
-        size=context_query_aware.get_shape()[-1].value,
+        size=100,
         name='context_query_aware_lstm'
     )
 
@@ -115,4 +113,4 @@ def rasor_net(context, context_len, question, question_len, span2position, embed
 
     logits = tf.squeeze(logits * span_mask, axis=-1)
 
-    return logits
+    return logits, context_query_aware_lstm, context_query_aware